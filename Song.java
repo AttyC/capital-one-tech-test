25bf649f4d54edd41b3c18cec99dfe2d644e4832
Antonia Cronin <antoniajcronin@gmail.com>
2018-05-02 13:16:29 +0100

    test for Song constructor

---
+++ Song.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Song {
+	
+	String title;
+	String artist;
+	String album;
+	int year;
+	
+   public Song(String title, String artist, String album, int year) {
+      this.title = title;
+      this.artist = artist;
+      this.album = album;
+      this.year = year;
+   }
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	
+	public String getAlbum() {
+		return album;
+	}
+	
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+}
--- SongTest.java
+++ SongTest.java
@@ -2,13 +2,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SongTest {
-	
-	int year = 2007;
-    Song song = new Song("One More Time", "Alison Moyet", "The Turn", 2007 );
-
-    @Test
-	public void getSongByYear() {
-//    (song.getSongByYear(year));
-	}
-   
+  @Test
+  public void testDefaultConstructor(){
+	Song song = new Song("One More Time", "Alison Moyet", "The Turn", 2007 );
+    assertEquals(song.getTitle(),"One More Time");
+    assertEquals(song.getArtist(),"Alison Moyet");
+    assertEquals(song.getAlbum(),"The Turn");
+    assertEquals(song.getYear(),2007);
+  }
+  
 }
+	
+	
+	
+
+ 
