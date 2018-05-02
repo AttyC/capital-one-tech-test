94f2688bb498b44e22e3b4cde88f7eab35b74b74
Antonia Cronin <antoniajcronin@gmail.com>
2018-05-02 11:58:17 +0100

    basic test class set up

---
+++ SongTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SongTest {
+	
+	int year = 2007;
+    Song song = new Song("One More Time", "Alison Moyet", "The Turn", 2007 );
+
+    @Test
+	public void getSongByYear() {
+//    (song.getSongByYear(year));
+	}
+   
+}
