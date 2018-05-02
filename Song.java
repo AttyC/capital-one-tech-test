a10f455ba281e19250f5ff45161c6979225270a0
Antonia Cronin <antoniajcronin@gmail.com>
2018-05-02 17:00:12 +0100

    remove unused setter methods

--- Song.java
+++ Song.java
@@ -13,35 +13,22 @@ public class Song {
       this.album = album;
       this.year = year;
    }
+   public static void main(String args[]) {} 
 	
 	public String getTitle() {
 		return title;
 	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
+
 	public String getArtist() {
 		return artist;
-	}
-	
-	public void setArtist(String artist) {
-		this.artist = artist;
 	}
 	
 	public String getAlbum() {
 		return album;
 	}
 	
-	public void setAlbum(String album) {
-		this.album = album;
-	}
 	public int getYear() {
 		return year;
 	}
-	
-	public void setYear(int year) {
-		this.year = year;
-	}
+
 }
